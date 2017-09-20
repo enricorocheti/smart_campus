faa2e41cd810f2847446d171a4f7f8d72ae6b014
enricorocheti <enricorocheti@gmail.com>
2017-09-19 21:17:38 -0300

    Subindo arquivo

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
+
+    setTimeout(function() {
+        $("#home").trigger('click');
+    },10);
+
     $('nav a#toggle').click(function () {
         $('ul').slideToggle(200, function () { });
     });
@@ -37,50 +42,50 @@ $(document).ready(function () {
         $('.content').append(html);
     });
 
-    $('#home').click(function () {
-        $('ul').slideToggle(200, function () { });
+    $('#home').click(function ($event) {
+        if ($event.originalEvent) $('ul').slideToggle(200, function () { });
         var html = '';
         $('.content').empty();
         html += "<h3 class='title'>Controle de Luminosidade Inteligente</h3>";
-		html += "<div>&nbsp;</div>";
-		html += "<div class='content-text'>Conectado a rede: <strong>Eduroam</strong></div>";
-		html += "<div>&nbsp;</div>";
-		html += "<div class='content-text'>Endere&ccedil;o (IP): <strong>192.168.1.53</strong></div>";
         html += "<div>&nbsp;</div>";
-		html += "<hr>";
-		html += "<div>&nbsp;</div>";
-		html += "<div class='content-text'>Modo de opera&ccedil;&atilde;o:</div>";
-		html += "<input type='radio' name='op-mode' value='auto' class='input-radio' checked><span class='input-radio'>Autom&aacute;tico</span><br>";
-		html += "<input type='radio' name='op-mode' value='manual' class='input-radio'><span class='input-radio'>Manual</span><br>";
-		html += "<div class='op-mode-manual'>";
-		html += "	<div>&nbsp;</div>";
-		html += "	<div>Pot&ecirc;ncia da l&acirc;mpada: <span id='slider-value'>50</span>%</div>";
-		html += "	<div id='slider-range'></div>";
-		html += "	<div style='float:left; margin-top:5px;'>0%</div><div style='float: right;margin-top:5px;'>100%</div>";
-		html += "	<div>&nbsp;</div>";
-		html += "</div>";
-		html += "<div>&nbsp;</div>";
-		html += "<hr>";
+        html += "<div class='content-text'>Conectado a rede: <strong>Eduroam</strong></div>";
+        html += "<div>&nbsp;</div>";
+        html += "<div class='content-text'>Endere&ccedil;o (IP): <strong>192.168.1.53</strong></div>";
+        html += "<div>&nbsp;</div>";
+        html += "<hr>";
+        html += "<div>&nbsp;</div>";
+        html += "<div class='content-text'>Modo de opera&ccedil;&atilde;o:</div>";
+        html += "<input type='radio' name='op-mode' value='auto' class='input-radio' checked><span class='input-radio'>Autom&aacute;tico</span><br>";
+        html += "<input type='radio' name='op-mode' value='manual' class='input-radio'><span class='input-radio'>Manual</span><br>";
+        html += "<div class='op-mode-manual'>";
+        html += "	<div>&nbsp;</div>";
+        html += "	<div>Pot&ecirc;ncia da l&acirc;mpada: <span id='slider-value'>50</span>%</div>";
+        html += "	<div id='slider-range'></div>";
+        html += "	<div style='float:left; margin-top:5px;'>0%</div><div style='float: right;margin-top:5px;'>100%</div>";
+        html += "	<div>&nbsp;</div>";
+        html += "</div>";
+        html += "<div>&nbsp;</div>";
+        html += "<hr>";
         $('.content').append(html);
-    });
 
-    $('#slider-range').slider({
-        value: 50,
-        min: 0,
-        max: 100,
-        animate: 'fast',
-        change: function (event, ui) {
-            // AQUI DEVE ENTRAR O CÓDIGO location.href PARA REDIRECIONAR PARA O ARDUINO
-            $('#slider-value').empty();
-            $('#slider-value').append(ui.value);
-        }
-    });
+        $('#slider-range').slider({
+            value: 50,
+            min: 0,
+            max: 100,
+            animate: 'fast',
+            change: function (event, ui) {
+                // AQUI DEVE ENTRAR O CÓDIGO location.href PARA REDIRECIONAR PARA O ARDUINO
+                $('#slider-value').empty();
+                $('#slider-value').append(ui.value);
+            }
+        });
 
-    $('input[type=radio][name=op-mode]').change(function () {
-        if (this.value == 'manual') {
-            $('.op-mode-manual').css('display', 'inline');
-        } else {
-            $('.op-mode-manual').css('display', 'none');
-        }
+        $('input[type=radio][name=op-mode]').change(function () {
+            if (this.value == 'manual') {
+                $('.op-mode-manual').css('display', 'inline');
+            } else {
+                $('.op-mode-manual').css('display', 'none');
+            }
+        });
     });
 });
