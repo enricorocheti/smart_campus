5d03582f9365dcd8d80cac0d05ae4b82600cec91
enricorocheti <enricorocheti@gmail.com>
2017-09-28 09:11:31 -0300

    Subindo código

--- main.js
+++ main.js
@@ -8,43 +8,46 @@ $(document).ready(function () {
     
             $('ul').slideToggle(200, function () { });
     
-            var html = '';
-            html += '<h3 class="title">Sobre</h3>';
-            html += '<div>&nbsp;</div>';
-            html += '<div class="content-text">';
-            html += '	&nbsp;&nbsp;&nbsp;Desenvolvido pelos alunos Diogo Schuarz, Enrico Oliveira Rocheti, Lucas de Luca, Pedro Dedin Neto e ';
-            html += '	Victor Marques de Souza Cardoso atrav&eacute;s do 1&ordm; Concurso Smart Campus criado pela Faculdade de Engenharia El&eacute;trica e de ';
-            html += '	Computa&ccedil;&atilde;o da UNICAMP. ';
-            html += '</div>';
-            html += '<div>&nbsp;</div>';
-    
-            write_content(html);
+            $('.content_home').css('display','none');
+            $('.content_chart').css('display','none');
+            $('.content_help').css('display','none');
+            $('.content_about').css('display','inline');
+
         });
     
         $('#help').click(function () {
     
             $('ul').slideToggle(200, function () { });
     
-            var html = '';
-            html += '<h3 class="title">Ajuda</h3>';
-            html += '<div>&nbsp;</div>';
-            html += '<div class="content-text">';
-            html += '	&nbsp;&nbsp;&nbsp;Na guia &quot;Principal&quot; voc&ecirc; consegue escolher o modo de opera&ccedil;&atilde;o do CLI: manual, ';
-            html += '	onde voc&ecirc; mesmo escolhe a luminosidade em porcentagem que a l&acirc;mpada deve permanecer acesa, ou autom&aacute;tico, onde uma leitura ';
-            html += '	da luminosidade do ambiente &eacute; realizada e, com base nisso, a l&acirc;mpada acende com uma pot&ecirc;ncia determinada por um algoritmo de luminosidade.';
-            html += '</div>';
-            html += '<div>&nbsp;</div>';
-            html += '<div class="content-text">';
-            html += '	&nbsp;&nbsp;&nbsp; Na guia &quot;Gr&aacute;ficos&quot; voc&ecirc; pode visualizar a pot&ecirc;ncia demandada pelas l&acirc;mpadas em um';
-            html += '	per&iacute;odo de 24 horas.';
-            html += '</div>';
+            $('.content_home').css('display','none');
+            $('.content_chart').css('display','none');
+            $('.content_help').css('display','inline');
+            $('.content_about').css('display','none');
+        });
+
+        $('#home').click(function () {
     
-            write_content(html);
+            $('ul').slideToggle(200, function () { });
+    
+            $('.content_home').css('display','inline');
+            $('.content_chart').css('display','none');
+            $('.content_help').css('display','none');
+            $('.content_about').css('display','none');
+        });
+
+        $('#chart').click(function () {
+    
+            $('ul').slideToggle(200, function () { });
+    
+            $('.content_home').css('display','none');
+            $('.content_chart').css('display','inline');
+            $('.content_help').css('display','none');
+            $('.content_about').css('display','none');
         });
     
     });
 
-    function getTime() {
+    /*function getTime() {
         return ['0h15','0h30','0h45','1h00','1h15','1h30','1h45','2h00','2h15','2h30','2h45','3h00',
         '3h15','3h30','3h45','4h00','4h15','4h30','4h45','5h00','5h15','5h30','5h45','6h00',
         '6h15','6h30','6h45','7h00','7h15','7h30','7h45','8h00','8h15','8h30','8h45','9h00',
@@ -53,7 +56,7 @@ $(document).ready(function () {
         '15h15','15h30','15h45','16h00','16h15','16h30','16h45','17h00','17h15','17h30','17h45','18h00',
         '18h15','18h30','18h45','19h00','19h15','19h30','19h45','20h00','20h15','20h30','20h45','21h00',
         '21h15','21h30','21h45','22h00','22h15','22h30','22h45','23h00','23h15','23h30','23h45','0h00']
-    }
+    }*/
     
     function write_content(html, page = '') {
     
